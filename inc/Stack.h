b52bdf7806488f91cfc9f3e0e9e2796500e334d1
Kobe Furner <kobefurner@mail.weber.edu>
2024-03-21 16:23:36 -0600

    Completed functions. All tests are passing

--- inc/Stack.h
+++ inc/Stack.h
@@ -1,6 +1,6 @@
 #ifndef ORDEREDLINKEDLIST_H
 #define ORDEREDLINKEDLIST_H
-#include <iostream>
+#include <iostream> 
 
 template <class Type>
 struct Node
@@ -34,77 +34,175 @@ public:
   Type topPop();
   void clear();
 private:
+  Node<Type>* head;
+  int count;
+  void printStack(Node<Type>* node, std::ostream& os) const;
 };
 
 template <class Type>
 Stack<Type>::Stack()
 {
+  head = nullptr;
+  count = 0;
 }
 
 template <class Type>
 Stack<Type>::Stack(const Stack<Type>& other)
 {
+  head = nullptr;
+  count = 0;
+
+  Stack<Type> tempStack;
+  Node<Type>* temp = other.head;
+  while (temp != nullptr)
+  {
+    tempStack.push(temp->data);
+    temp = temp->next;
+  }
+
+  temp = tempStack.head;
+  while (temp != nullptr)
+  {
+    push(temp->data);
+    temp = temp->next;
+  }
 }
 
 template <class Type>
 Stack<Type>& Stack<Type>::operator=(const Stack& other)
 {
+  if (this != &other)
+  {
+    clear();
+    Stack<Type> tempStack;
+    Node<Type>* temp = other.head;
+    while (temp != nullptr)
+    {
+      tempStack.push(temp->data);
+      temp = temp->next;
+    }
+
+    temp = tempStack.head;
+    while (temp != nullptr)
+    {
+      push(temp->data);
+      temp = temp->next;
+    }
+  }
   return *this;
 }
 
 template <class Type>
 Stack<Type>::~Stack()
 {
+  clear();
 }
 
 template <class Type>
 int Stack<Type>::size() const
 {
-  return -1;
+  return count;
 }
 
 template <class Type>
 bool Stack<Type>::empty() const
 {
-  return -1;
+  if (count == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 template <class Type>
 Type Stack<Type>::top() const
 {
-  Type ret;
-  return ret;
+  if (head != nullptr){
+    return head->data;
+  }
+  else {
+    throw std::logic_error("Stack is empty");
+  }
 }
 
 template <class Type>
 void Stack<Type>::push(const Type& item)
 {
+  Node<Type> * newNode = new Node<Type>;
+  newNode->data = item;
+  newNode->next = head;
+  head = newNode;
+  count++;
 }
 
 template <class Type>
 void Stack<Type>::pop()
 {
+  if (head != nullptr){
+    Node<Type>* temp = head;
+    head = head->next;
+    delete temp;
+    count--;
+  }
+  else {
+    return;
+  }
 }
 
 template <class Type>
 void Stack<Type>::pop(int count)
 {
+  for (int i = 0; i < count; i++){
+    pop();
+  }
+  if(empty()){
+    return;
+  }
 }
 
 template <class Type>
 Type Stack<Type>::topPop()
 {
-  Type ret;
-  return ret;
+  if (head != nullptr){
+    Type ret = head->data;
+    pop();
+    return ret;
+  }
+  else {
+    throw std::logic_error("Stack is empty");
+  
+  }
 }
 
 template <class Type>
 void Stack<Type>::clear()
 {
+  while (head != nullptr){
+    Node<Type>* temp = head;
+    head = head->next;
+    delete temp;
+  }
+  count = 0;
+}
+
+template <class Type>
+void Stack<Type>::printStack(Node<Type>* node, std::ostream& os) const
+{
+  if (node == nullptr){
+    return;
+  }
+  printStack(node->next, os);
+  if (node->next != nullptr){
+    os << "->";
+  }
+  os << node->data;
 }
 
 template <class Type>
 std::ostream& operator<<(std::ostream& out, const Stack<Type>& list)
 {
+  list.printStack(list.head, out);
+  return out;
 }
 #endif
--- src/ParenthesesMatch.cpp
+++ src/ParenthesesMatch.cpp
@@ -1,5 +1,17 @@
 #include "ParenthesesMatch.h"
 
 bool parenthesesMatch(const char* str){
-   return false;
+   Stack<char> stack;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(') {
+            stack.push(str[i]);
+        } 
+        else if (str[i] == ')') {
+            if (stack.empty()) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.empty();
 }
